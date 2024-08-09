c1de7f70d730c39403b45ce292b8a0c80ff0b413
matvey0479@mail.ru <matvey0479@mail.ru>
2024-08-09 22:29:42 +0700

    first

---
+++ Shop/DataManager.cs
@@ -0,0 +1,136 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    public class DataManager
+    {
+        public DataManager(string connectionSring)
+        {
+            this.connectionString = connectionSring;
+        }
+        string connectionString;
+
+        public async Task LoadData(string XmlPath)
+        {
+            try
+            {
+                await AddUsers(XmlPath);
+                await AddProducts(XmlPath);
+                await AddOrders(XmlPath);
+                await AddOrdersProducts(XmlPath);
+                Console.WriteLine();
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+        public async Task AddUsers(string XmlPath)
+        {
+            string sqlExpression = "InsertUsers";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@XmlPath",
+                    Value = XmlPath
+                };
+                command.Parameters.Add(nameParam);
+                int number = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("В таблицу Users добавлено объектов: {0}", number);
+
+            }
+        }
+        public async Task AddProducts(string XmlPath)
+        {
+            string sqlExpression = "InsertProducts";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@XmlPath",
+                    Value = XmlPath
+                };
+                command.Parameters.Add(nameParam);
+
+                int number = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("В таблицу Products добавлено объектов: {0}", number);
+
+            }
+        }
+        public async Task AddOrders(string XmlPath)
+        {
+            string sqlExpression = "InsertOrders";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@XmlPath",
+                    Value = XmlPath
+                };
+                command.Parameters.Add(nameParam);
+                int number = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("В таблицу Orders добавлено объектов: {0}", number);
+
+            }
+        }
+        public async Task AddOrdersProducts(string XmlPath)
+        {
+            string sqlExpression = "InsertOrdersProducts";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlParameter nameParam = new SqlParameter
+                {
+                    ParameterName = "@XmlPath",
+                    Value = XmlPath
+                };
+                command.Parameters.Add(nameParam);
+                int number = await command.ExecuteNonQueryAsync();
+                Console.WriteLine("В таблицу OrdersProducts добавлено объектов: {0}", number);
+
+            }
+        }
+        public async Task GetSales()
+        {
+            string sqlExpression = "GetSales";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                command.CommandType = CommandType.StoredProcedure;
+                using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                {
+                    if (reader.HasRows)
+                    {
+                        Console.WriteLine($"{reader.GetName(0)}\t{reader.GetName(1)}\t{reader.GetName(2)}");
+
+                        while (await reader.ReadAsync())
+                        {
+                            string NameProduct = reader.GetString(0);
+                            double Quantity = reader.GetDouble(1);
+                            double Amount = reader.GetDouble(2);
+                            Console.WriteLine($"{NameProduct} \t{Quantity} \t\t{Amount}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Test;
+
+string connectionString = "Server=HOME-PC;Database=Shop;Trusted_Connection=True;";
+DataManager dataManager = new DataManager(connectionString);
+Console.Write("Выберите действие:\n" +
+                "1) Загрузить данные из XML\n" +
+                "2) Отобразить список продаж\n" +
+                "3) Закрыть программу\n\n");
+
+bool state = true;
+while (state)
+{
+    switch (Console.ReadLine())
+    {
+        case "1":
+            Console.WriteLine("Введите путь до файла:");
+            string xmlPath = Console.ReadLine().ToString();
+            await dataManager.LoadData(xmlPath);
+            break;
+        case "2":
+            await dataManager.GetSales();
+            break;
+        case "3":
+            state = false;
+            Environment.Exit(0);
+            break;
+        default:
+            Console.WriteLine("Некорректный ввод!");
+            break;
+    }
+
+}
+Console.ReadLine();
+
+
+
